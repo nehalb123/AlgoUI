01af58d02ca2d9165a0312f5d9c2e1caa50ef54e
nehalb123 <nehal.borole@gmail.com>
2020-03-04 01:34:56 +0530

    Changes in editor

--- src/client/problems/components/AceCodeEditor.tsx
+++ src/client/problems/components/AceCodeEditor.tsx
@@ -3,7 +3,7 @@ import 'brace';
 import AceEditor from 'react-ace';
 
 import 'brace/mode/java';
-import 'brace/theme/tomorrow_night_eighties';
+import 'brace/theme/monokai';
 import 'brace/ext/language_tools';
 import {CSSProperties} from "react";
 
@@ -25,7 +25,8 @@ export default class AceCodeEditor extends React.Component<AceCodeEditorProps, {
         return <div style={editorStyle}>
             <AceEditor
                 mode="java"
-                theme="tomorrow_night_eighties"
+                theme="github"
+                fontSize={16}
                 name="editor"
                 value={this.props.sourceCode}
                 onChange={this.props.onSourceCodeChanged}
